2ae95a136886c16c6a11a603813f26dce9538d7d
Jarod Peachey <jwpeachey107@aol.com>
2019-10-15 19:40:39 -0400

    State before convert to class based

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,32 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link
-          to="/"
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
+const Header = ({ siteTitle }) => {
+  return (
+    <header className="bg-primary">
+      <div className="navbar-content">
+        <div className="navbar-left">
+          <h2>
+            {siteTitle}
+          </h2>
+        </div>
+        <div className="navbar-right">
+          <ul className="menu">
+            <li className="menu-item">
+              <a href="/">Link</a>
+            </li>
+            <li className="menu-item">
+              <a href="/">Link</a>
+            </li>
+            <li className="menu-item">
+              <a href="/">Link</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
