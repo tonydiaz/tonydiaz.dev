7a70633baa8f544cc771ae07660224c82ae03e11
Jarod Peachey <jwpeachey107@aol.com>
2019-10-15 20:55:44 -0400

    Add font and navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,43 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, setState } from 'react';
 
 const Header = ({ siteTitle }) => {
+  const [headerScrolled, setHeaderState] = useState(false);
+
+  const onScroll = () => {
+    if (window.scrollY > 10) {
+      setHeaderState(true);
+    } else {
+      setHeaderState(false);
+    }
+  };
+
+  window.addEventListener('scroll', onScroll);
+
   return (
-    <header className="bg-primary">
-      <div className="navbar-content">
-        <div className="navbar-left">
-          <h2>
-            {siteTitle}
-          </h2>
-        </div>
-        <div className="navbar-right">
-          <ul className="menu">
-            <li className="menu-item">
-              <a href="/">Link</a>
-            </li>
-            <li className="menu-item">
-              <a href="/">Link</a>
-            </li>
-            <li className="menu-item">
-              <a href="/">Link</a>
-            </li>
-          </ul>
+    <header
+      id="navbar"
+      className={headerScrolled ? 'bg-primary' : 'bg-transparent'}
+    >
+      <div className="px-sm">
+        <div className="navbar-content">
+          <div className="navbar-left">
+            <h2>{siteTitle}</h2>
+          </div>
+          <div className="navbar-right">
+            <ul className="menu">
+              <li className="menu-item">
+                <a href="/">Link</a>
+              </li>
+              <li className="menu-item">
+                <a href="/">Link</a>
+              </li>
+              <li className="menu-item">
+                <a href="/">Link</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,21 @@ import SEO from '../components/seo';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people!</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div>
-      <Image />
+    <div className="hero">
+      <div className="hero_content container">
+        <h1 className="hero_title">Jarod Peachey</h1>
+        <h4 className="hero_subtitle">Front End Web Developer and Designer</h4>
+        <div className="hero_buttons">
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="container">
+      <h3 className="title">
+        About Me
+      </h3>
+    </div>
   </Layout>
 );
 
