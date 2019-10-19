e9f9f777ca69d7dc1e5fcc44f1884623c4c984c4
Jarod Peachey <jwpeachey107@aol.com>
2019-10-19 13:46:05 -0400

    Update skills section

--- src/components/Homepage/skills_section.js
+++ src/components/Homepage/skills_section.js
@@ -1,85 +1,45 @@
 import React from 'react';
-import { useStaticQuery, StaticQuery, graphql } from 'gatsby';
-import { SkillsImageOne, SkillsImageTwo, SkillsImageThree } from '../image';
 import { styled } from 'linaria/react';
 
 const SkillsSection = () => (
   <section className="bg-light" id="skills">
     <div className="container">
       <h2 className="title">Skills</h2>
-      <div className="row mobile-lg">
-        <div className="col col-6 desktop-col-4">
-          <SkillsCard>
-            <SkillsIcon>
-              <SkillsImageOne />
-            </SkillsIcon>
-            <SkillsTitle>Responsive Design</SkillsTitle>
-            <SkillsInfo>
-              Responsive design is at the core of my websites. I strive to make
-              every website look beautiful and function easily on EVERY screen
-              size.
-            </SkillsInfo>
-          </SkillsCard>
+      <div className="row">
+        <div className="col col-4">
+          <p>Flsakdfjs lskjo i jl, I kjasdfl nkj ljksj df. I kslfdl nh fls. Ekfliusj l kljsfdlk, klsdjfljf, lksdjflkjflsdf - sdfkj. lskdjfs lkjfasdljfsd. KJDFLjlkj, lksjdaflkjdf.</p>
         </div>
-        <div className="col col-6 desktop-col-4">
-          <SkillsCard>
-            <SkillsIcon>
-              <SkillsImageTwo />
-            </SkillsIcon>
-            <SkillsTitle>Responsive Design</SkillsTitle>
-            <SkillsInfo>
-              Responsive design is at the core of my websites. I strive to make
-              every website look beautiful and function easily on EVERY screen
-              size.
-            </SkillsInfo>
-          </SkillsCard>
-        </div>
-        <div className="col col-6 desktop-col-4">
-          <SkillsCard>
-            <SkillsIcon>
-              <SkillsImageThree />
-            </SkillsIcon>
-            <SkillsTitle>Responsive Design</SkillsTitle>
-            <SkillsInfo>
-              Responsive design is at the core of my websites. I strive to make
-              every website look beautiful and function easily on EVERY screen
-              size.
-            </SkillsInfo>
-          </SkillsCard>
+        <div className="col col-8">
+          <div className="row mobile-lg">
+            <div className="col col-2">
+              <ProgressTitle>HTML</ProgressTitle>
+            </div>
+            <div className="col col-10">
+              <Progress>
+                <ProgressBar value={90} />
+              </Progress>
+            </div>
+          </div>         
         </div>
       </div>
+
     </div>
   </section>
 );
 
-const SkillsCard = styled.div`
-  padding: 24px;
-  // border: 1px solid #ddd;
-  // background: white;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  text-align: center;
+const Progress = styled.div`
+  background: white;
+  border-radius: 50px;
 `;
 
-const SkillsIcon = styled.div`
-  margin: 0 auto;
-  width: 120px;
-  margin-bottom: 12px;
-`;
+const ProgressTitle = styled.div``;
 
-const SkillsTitle = styled.div`
-  font-size: 22px;
-  color: var(--primary);
-  font-weight: 600;
-  margin-bottom: 8px;
+const ProgressBar = styled.div`
+  height: 20px;
+  width: ${props => props.value}%;
+  background: orange;
+  border-top-left-radius: 50px;
+  border-bottom-left-radius: 50px;
 `;
-
-const SkillsInfo = styled.div`
-  font-size: 16px;
-`;
-
 
 export default SkillsSection;
