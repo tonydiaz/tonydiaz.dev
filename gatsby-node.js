f1b0efcc21e52716c7c6c586eec8baa7422e91d2
Jarod Peachey <jwpeachey107@aol.com>
2019-10-21 16:42:24 -0400

    Add gzip?

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+// in gatsby-node.js
+import fs from 'fs'; // native
+import zlib from 'zlib'; // native
+import path from 'path'; // native
+import glob from 'glob'; // https://www.npmjs.com/package/glob
+
+export default function postBuild (pages, callback) {
+  const publicPath = path.join(__dirname, 'public');
+  const gzippable = glob.sync(`${publicPath}/**/?(*.html|*.js|*.css)`);
+  gzippable.forEach((file) => {
+    const content = fs.readFileSync(file);
+    const zipped = zlib.gzipSync(content);
+    fs.writeFileSync(`${file}.gz`, zipped);
+  });
+  callback();
+}
